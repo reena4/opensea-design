c680998f87b77959eb403bc9ff2e2b62b2359bc7
reena4 <reenu4790@gmail.com>
2022-10-11 17:15:07 +0530

    footer

--- src/Components/layout/footer/Footer.js
+++ src/Components/layout/footer/Footer.js
@@ -1,43 +1,116 @@
 import React from 'react'
 import { Container,Row,Col,Form,Button } from 'react-bootstrap'
 import "./footer.css";
+//import { SocialIcon } from 'react-social-icons';
+import {BsTwitter , BsInstagram,BsYoutube} from 'react-icons/bs'
+import{FaRedditAlien,FaTiktok,FaDiscord, FaRegEnvelope} from 'react-icons/fa'
+import logo from "../../../images/opensea-white.svg"
 const Footer = () => {
   return (
     <div>
     <footer>
-  <Container fluid className='bg-secondary ' >
-  <Container className='p-4'>
-    <Row>
+  <Container fluid className='bg-primary ' >
+  <Container className='p-3'>
+    <Row className='mt-5'>
     <Col md={6} >
-      <h4>Stay in the loop</h4>
+      <h5><b>Stay in the loop</b></h5>
       <p>Join our mailing list to stay in the loop with our newest feature releases, NFT drops, and tips and tricks for navigating OpenSea.</p>
-      <Form className="d-flex formwidth">
+      <Form className="d-flex ">
       
        
       <Form.Control
               type="search"
-              placeholder="Email"
-              className="me-2"
+              placeholder="Enter your email"
+              className="me-2 formwidth"
               aria-label="Search"
             />
         
-        <Button >Search</Button>
+        <Button className="search">Search</Button>
       
      
       </Form>
     </Col>
     
     <Col md={6}  >
-    <h4>Join the community</h4>
-    <Button class="btn"><i class="fa-brands fa-twitter"></i></Button>
-<Button class="btn"><i class="fa fa-bars"></i></Button>
-<Button class="btn"><i class="fa fa-trash"></i></Button>
-<Button class="btn"><i class="fa fa-close"></i></Button>
-<Button class="btn"><i class="fa fa-folder"></i></Button>
+    <h5><strong>Join the community</strong></h5>
+    <Button className="btn-size" ><BsTwitter className="iconsize"/></Button>
+<Button className="btn-size"><BsInstagram className="iconsize"/></Button>
+<Button className='btn-size'><FaDiscord className="iconsize"/></Button>
+<Button className="btn-size"><FaRedditAlien className="iconsize"/></Button>
+<Button className="btn-size"><BsYoutube className="iconsize"/></Button>
+<Button className="btn-size"><FaTiktok className="iconsize"/></Button>
+<Button className='btn-size'><FaRegEnvelope className="iconsize"/></Button>
     </Col>
    
     </Row>
-    </Container>
+    
+  
+   <div className='horizontal_line mt-5 mb-5'/>
+   <Row  >
+   
+        <Col md={4}>
+        <img src={logo} alt="/" style={{width:40}} />
+        <h5><strong>OpenSea</strong></h5>
+        <p className='footer_paragraph'>The world’s first and largest digital marketplace for crypto collectibles and non-fungible tokens (NFTs). Buy, sell, and discover exclusive digital items.</p>
+        </Col>
+        <Col xs lg="2">
+        <h6 className='heading'><strong>Marketplace</strong></h6>
+        <ul>
+          <li>All NFTs</li>
+          <li>Solana NFTs</li>
+          <li>Art</li>
+          <li>Collectibles</li>
+          <li>Domain Names</li>
+          <li>Music</li>
+          <li>Photography</li>
+          <li>Sports</li>
+          <li>Trading Cards</li>
+          <li>Utility</li>
+          <li>Virtual Worlds</li>
+        </ul>
+        </Col>
+        <Col xs lg="2">
+       <h6><strong>My Account</strong></h6>
+       <ul>
+        <li>Profile</li>
+        <li>Favorites</li>
+        <li>Watchlist</li>
+        <li>My Collections</li>
+        <br/>
+        <br/>
+        <h6><strong>Stats</strong></h6>
+        <li>Rankings</li>
+        <li>Activity</li>
+       </ul>
+        </Col>
+        <Col xs lg="2">
+        <h6><strong>Resources</strong></h6>
+        <ul>
+          <li>Learn</li>
+          <li>Help Center</li>
+          <li>Platform Status</li>
+          <li>Partners</li>
+          <li>Taxes</li>
+          <li>Blog</li>
+          <li>Docs</li>
+          <li>Newsletter</li>
+        </ul>
+        </Col>
+        <Col xs lg="2">
+       <h6> <strong>Company</strong></h6>
+       <ul>
+       <li>Careers</li>
+       <li>Ventures</li>
+       <li>Grants</li>
+        </ul>
+        </Col>
+   </Row>
+   <div className='horizontal_line mt-5 mb-4'/>
+   <Row>
+    <Col md={6} className="copyright">© 2018 - 2022 Ozone Networks, Inc</Col>
+    <Col md={6}>f</Col>
+   </Row>
+   </Container>
   </Container>
   </footer>
           </div>
